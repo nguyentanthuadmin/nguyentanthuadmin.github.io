3acad6b4f28a9206e9c1a7a892dd3e6f3aad4a08
nhysoft <113497087+vietbestech@users.noreply.github.com>
2022-11-04 01:09:33 +0700

    admin nguyen tan thu

---
+++ precache-manifest.2fb42bef8653d09d75d26b1cc5e88a6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3cb8bbc5b2f971f1d598b60a462e83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23e27226430fad8500b0",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1054c611f2c0a13fc893",
+    "url": "/static/css/main.345bd126.chunk.css"
+  },
+  {
+    "revision": "23e27226430fad8500b0",
+    "url": "/static/js/2.1f4e29cf.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.1f4e29cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1054c611f2c0a13fc893",
+    "url": "/static/js/main.79809f75.chunk.js"
+  },
+  {
+    "revision": "55d488e9e81e555a03c4",
+    "url": "/static/js/runtime-main.077a2d11.js"
+  }
+]);
